4135a641542e86d54d9e4a1a3e9bc6e1ff32b3f5
Minje <alswp625@naver.com>
2020-07-01 23:58:52 +0900

    Solve ex1926

---
+++ ex1926_간단한_369게임_D2/ex1926.java
@@ -0,0 +1,30 @@
+package ex1926_간단한_369게임_D2;
+import java.util.Scanner;
+//2020.06.23 댓글보고 해결....
+
+public class ex1926 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String number = sc.next();
+		
+		for(int i=1; i<=Integer.parseInt(number); i++) {
+			String num = i + "";
+			
+			String [] numArr = num.split("");
+			String result = "";
+			
+			for(int j=0; j<numArr.length; j++) {
+				if(numArr[j].equals("3") || numArr[j].equals("6") 
+						|| numArr[j].equals("9")) {
+					result = result + "-";
+				}
+			}
+			
+			if(result.equals("")) {
+				result = num;
+			}
+			
+			System.out.print(result + " ");
+		}
+	}
+}
