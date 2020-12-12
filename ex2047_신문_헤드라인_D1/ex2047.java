b0de4b20b4d481881be76c985b6589c0abbc687d
Minje <alswp625@naver.com>
2020-12-12 17:14:06 +0900

    Solve ex2047

---
+++ ex2047_신문_헤드라인_D1/ex2047.java
@@ -0,0 +1,11 @@
+package ex2047_신문_헤드라인_D1;
+import java.util.Scanner;
+
+public class ex2047 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String string = sc.next();
+		
+		System.out.println(string.toUpperCase());
+	}
+}
